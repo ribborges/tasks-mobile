44f2b023ce7dcb56479fab738b7200dfc9f76deb
Richard Borges <ribborges@outlook.com>
2025-02-06 13:18:24 -0300

    feat: simplify Loading component by replacing animated icon with ActivityIndicator

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,29 +1,11 @@
-import { View, Animated, Image, Easing, StatusBar, useColorScheme } from 'react-native';
-import { useEffect, useRef } from 'react';
-
-import { ArrowRepeat } from '@/components/Icons';
+import { View, Image, StatusBar, useColorScheme, ActivityIndicator } from 'react-native';
 
 export default function Loading() {
     const colorScheme = useColorScheme();
-    const rotateValue = useRef(new Animated.Value(0)).current;
-
-    useEffect(() => {
-        // Rotate animation
-        Animated.loop(
-            Animated.sequence([
-                Animated.timing(rotateValue, {
-                    toValue: 1,
-                    duration: 1000,
-                    useNativeDriver: true,
-                    easing: Easing.linear
-                })
-            ])
-        ).start();
-    }, [rotateValue]);
 
     return (
         <View className="
-            flex-1 justify-center items-center gap-5
+            flex-1 justify-center items-center gap-10
             bg-white dark:bg-black
         ">
             <StatusBar
@@ -34,21 +16,7 @@ export default function Loading() {
                 source={require('#/images/icon.png')}
                 className="w-24 h-24"
             />
-            <Animated.View className="
-                p-1
-                bg-zinc-200 dark:bg-zinc-900
-                border border-solid rounded-full
-                border-zinc-300 dark:border-zinc-800
-            " style={{
-                    transform: [{
-                        rotate: rotateValue.interpolate({
-                            inputRange: [0, 1],
-                            outputRange: ['0deg', '360deg'],
-                        })
-                    }]
-                }}>
-                <ArrowRepeat color="#71717a" size={32} />
-            </Animated.View>
+            <ActivityIndicator size="large" color="#71717a" />
         </View>
     );
 }
