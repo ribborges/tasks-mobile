9213086f24c8af66fd2287473498944ecc9b5e2a
Richard Borges <ribborges@outlook.com>
2025-02-06 11:03:16 -0300

    feat: update app name and version; add environment configuration and system UI background color handling

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,15 @@
 import { Slot } from "expo-router";
+import { useColorScheme } from "react-native";
+import * as SystemUI from 'expo-system-ui';
 
 import { SessionProvider } from "@/provider/SessionProvider";
 
 import '@/styles/global.css';
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  SystemUI.setBackgroundColorAsync(colorScheme === 'light' ? 'white' : 'black');
+
   return (
     <SessionProvider>
       <Slot />
---
+++ src/config/env.ts
@@ -0,0 +1,3 @@
+const apiURL = process.env.EXPO_PUBLIC_API_URL;
+
+export { apiURL };
