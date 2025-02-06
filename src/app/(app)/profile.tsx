a824ddcab98c525e57e7ace7b2d350ee31ed3285
Richard Borges <ribborges@outlook.com>
2025-02-06 17:30:59 -0300

    feat: implement profile component with user information and password management

---
+++ src/app/(app)/profile.tsx
@@ -0,0 +1,112 @@
+import { useState } from 'react';
+import { ScrollView, View } from 'react-native';
+import { FontAwesome, FontAwesome5, Ionicons } from '@expo/vector-icons';
+
+import { Spacer } from '@/components/Separator';
+import { ProfilePic, UserInfo } from '@/components/User';
+import { useUserStore } from '@/lib/store';
+import { Button, InputPassword, InputText } from '@/components/Input';
+
+export default function Profile() {
+    const { user, setUser } = useUserStore();
+
+    const [userData, setUserData] = useState({
+        name: user?.name || '',
+        username: user?.username || '',
+        email: user?.email || ''
+    });
+
+    const [password, setPassword] = useState({
+        password: "",
+        newPassword: ""
+    });
+
+    const onChange = (value: string, name: string) => {
+        setUserData((prevState: any) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    }
+
+    const onChangePass = (value: string, name: string) => {
+        setPassword((prevState: any) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    }
+
+    return (
+        <View className="
+            p-4
+            flex-1
+            bg-white dark:bg-black
+        ">
+            {user && (
+                <View className="flex-row items-center gap-4">
+                    <ProfilePic className="h-24 w-24" iconSize={32} />
+                    <UserInfo size="xl" name={user?.name} username={user?.username} />
+                </View>
+            )}
+            <Spacer space={30} />
+            <ScrollView className="
+                flex-1
+                bg-zinc-100 bg-opacity-50 dark:bg-zinc-900 dark:bg-opacity-50
+                border border-solid rounded-3xl
+                border-zinc-200 border-opacity-50 dark:border-zinc-800 dark:border-opacity-50
+            " contentContainerClassName="items-stretch p-4 gap-4">
+                <View className="gap-2">
+                    <InputText
+                        id='name'
+                        name='name'
+                        value={userData.name}
+                        onChange={onChange}
+                        icon={<Ionicons name="person" />}
+                        label='Name'
+                        placeholder="Ana Silva"
+                    />
+                    <InputText
+                        id='username'
+                        name='username'
+                        value={userData.username}
+                        onChange={onChange}
+                        icon={<FontAwesome5 name="id-badge" />}
+                        label='Username'
+                        placeholder="ana.Silva"
+                        autoCapitalize="none"
+                    />
+                    <InputText
+                        id='email'
+                        name='email'
+                        value={userData.email}
+                        onChange={onChange}
+                        icon={<FontAwesome name="envelope" />}
+                        label='Email'
+                        placeholder="ana_Silva@email.com"
+                        autoCapitalize="none"
+                    />
+                    <Button label='Save' />
+                </View>
+                <Spacer space={15} />
+                <View className="gap-2">
+                    <InputPassword
+                        id='password'
+                        name='password'
+                        value={password.password}
+                        onChange={onChangePass}
+                        icon={<Ionicons name="key" />}
+                        label='Password'
+                    />
+                    <InputPassword
+                        id='newPassword'
+                        name='newPassword'
+                        value={password.newPassword}
+                        onChange={onChangePass}
+                        icon={<Ionicons name="key" />}
+                        label='New password'
+                    />
+                    <Button label='Change password' />
+                </View>
+            </ScrollView>
+        </View>
+    );
+}
