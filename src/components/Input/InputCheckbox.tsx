7d091a679aa63a74dc9db4d18ef8a53a9dbf0c1b
Richard Borges <ribborges@outlook.com>
2025-02-07 16:07:07 -0300

    feat: add InputCheckbox component

---
+++ src/components/Input/InputCheckbox.tsx
@@ -0,0 +1,64 @@
+import { ReactNode } from "react";
+import { TouchableOpacity, View, Text } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import clsx from "clsx";
+
+interface CheckboxInputProps {
+    disabled?: boolean,
+    id?: string,
+    name?: string,
+    onChange?: (value: boolean, name: string) => void,
+    value?: boolean,
+    icon?: ReactNode,
+    placeholder?: string,
+    label?: string,
+}
+
+export default function InputCheckbox(props: CheckboxInputProps) {
+    const onChange = (value: boolean, name: string) => {
+        if (props.onChange) {
+            props.onChange(value, name);
+        }
+    }
+
+    return (
+        <View className="flex-row items-center gap-2">
+            <TouchableOpacity
+                id={props.id}
+                activeOpacity={0.5}
+                onPress={() => onChange(!props.value, props.name || '')}
+                className={clsx(
+                    `
+                        h-10 w-10
+                        m-1 p-1
+                        border-2 border-solid rounded-xl
+                    `, props.value ? "bg-indigo-500 border-indigo-800" : "border-zinc-400 dark:border-zinc-700"
+                )}
+            >
+                {
+                    props.value &&
+                    <View className="flex-row flex-1 items-center justify-center">
+                        <Text className="text-zinc-700 dark:text-zinc-100">
+                            <FontAwesome name="check" size={16} />
+                        </Text>
+                    </View>
+                }
+            </TouchableOpacity>
+            <View className="flex-row items-center gap-2">
+                <View>
+                    {
+                        props.icon &&
+                        <Text className="text-zinc-950 dark:text-zinc-100">
+                            {props.icon}
+                        </Text>
+                    }
+                </View>
+                <View>
+                    <Text className="text-zinc-950 dark:text-zinc-100">
+                        {props.label}
+                    </Text>
+                </View>
+            </View>
+        </View>
+    );
+}
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,7 +4,8 @@ import InputText from "./InputText";
 import InputPassword from "./InputPassword";
 import InputDate from "./InputDate";
 import InputSelector from "./InputSelector";
+import InputCheckbox from "./InputCheckbox";
 import OptionSelector from "./OptionSelector";
 import Button from "./Button";
 
-export { Label, InputGroup, InputText, InputPassword, InputDate, InputSelector, OptionSelector, Button };
+export { Label, InputGroup, InputText, InputPassword, InputDate, InputSelector, InputCheckbox, OptionSelector, Button };
