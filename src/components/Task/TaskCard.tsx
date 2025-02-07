a9d75f7774372587c7fcf8a1fe33c860e788d0df
Richard Borges <ribborges@outlook.com>
2025-02-07 04:02:55 -0300

    feat: add functionality to mark tasks as important and delete tasks in TaskCard component

--- src/components/Task/TaskCard.tsx
+++ src/components/Task/TaskCard.tsx
@@ -1,22 +1,55 @@
-import { TouchableOpacity, View, Text, useColorScheme } from "react-native";
+import { TouchableOpacity, View, Text } from "react-native";
 
 import { useCategoryStore, useTaskStore } from "@/lib/store";
 import Collapse from "@/components/Collapse";
 import { formatDate } from "@/utils/formatDate";
 import { FontAwesome } from "@expo/vector-icons";
+import { RemoveTask, UpdateTask } from "@/services/task.service";
+import { useRouter } from "expo-router";
 
 interface TaskCardProps {
     taskID: string;
 }
 
 export default function TaskCard(props: TaskCardProps) {
-    const colorScheme = useColorScheme();
-
     const { removeTask, updateTask, getTask } = useTaskStore();
     const { getCategory } = useCategoryStore();
 
+    const router = useRouter();
+
     const task = getTask(props?.taskID);
     const category = getCategory(task ? task.categoryId : '');
+
+    const setIsImportant = () => {
+        if (task) UpdateTask(task?.id, {
+            isImportant: !task.isImportant
+        })
+            .then(res => {
+                if (res?.status !== 200) {
+                    console.error('Error updating task:', res);
+                    return;
+                }
+
+                updateTask(task.id, {
+                    ...task,
+                    isImportant: !task.isImportant
+                });
+            })
+            .catch(error => console.error('There has been a problem with your fetch operation: ', error));
+    }
+
+    const deleteTask = () => {
+        if (task) RemoveTask(task.id)
+            .then(res => {
+                if (res?.status !== 200) {
+                    console.error('Error removing task:', res);
+                    return;
+                }
+
+                removeTask(task.id);
+            })
+            .catch(error => console.error('There has been a problem with your fetch operation: ', error));
+    };
 
     return (
         <View className="flex-row items-center gap-2 p-2 rounded-xl border border-solid border-zinc-300 dark:border-zinc-800">
@@ -38,17 +71,17 @@ export default function TaskCard(props: TaskCardProps) {
                 <Text className="text-gray-500">{`Created: ${formatDate('en-US', task?.createdAt)}`}</Text>
             </Collapse>
             <View className="flex-row items-center gap-1">
-                <TouchableOpacity className="p-1">
+                <TouchableOpacity onPress={setIsImportant} activeOpacity={0.5} className="p-1">
                     <Text className={task?.isImportant ? "text-yellow-500" : "text-zinc-700 dark:text-zinc-300"}>
                         <FontAwesome name={'star'} size={18} />
                     </Text>
                 </TouchableOpacity>
-                <TouchableOpacity className="p-1">
+                <TouchableOpacity onPress={() => router.navigate({ pathname: "/edit_task", params: { id: task?.id } })} activeOpacity={0.5} className="p-1">
                     <Text className="text-zinc-700 dark:text-zinc-300">
                         <FontAwesome name={'pencil-square'} size={18} />
                     </Text>
                 </TouchableOpacity>
-                <TouchableOpacity className="p-1">
+                <TouchableOpacity onPress={deleteTask} activeOpacity={0.5} className="p-1">
                     <Text className="text-zinc-700 dark:text-zinc-300">
                         <FontAwesome name={'trash'} size={18} />
                     </Text>
