fbc747acfd64087c4513a043338f9dadbd726504
Richard Borges <ribborges@outlook.com>
2025-02-07 04:02:17 -0300

    feat: add InputDate component for date and time selection

---
+++ src/components/Input/InputDate.tsx
@@ -0,0 +1,64 @@
+import React, { ReactNode, useState } from "react";
+import { TouchableOpacity, Text } from "react-native";
+import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
+
+import InputGroup from "./InputGroup";
+
+interface DateInputProps {
+    mode?: "date" | "time" | "datetime" | "countdown",
+    fakeInput?: boolean,
+    disabled?: boolean,
+    id?: string,
+    name?: string,
+    onChange?: (value: string, name: string) => void,
+    value?: string,
+    icon?: ReactNode,
+    placeholder?: string,
+    label?: string
+}
+
+export default function InputDate(props: DateInputProps) {
+    const [show, setShow] = useState(false);
+
+    const onChangeDate = (event: DateTimePickerEvent, selectedDate: Date | undefined) => {
+        if (selectedDate) {
+            const dateString = selectedDate.toISOString();
+            props.onChange && props.onChange(dateString, props.name || '');
+        }
+        setShow(false);
+    }
+
+    return (
+        <>
+            <InputGroup fakeInput={props.fakeInput} value={props.value} icon={props.icon} label={props.label}>
+                <TouchableOpacity activeOpacity={0.5} className="p-4" onPress={() => setShow(true)}>
+                    {
+                        props.value ?
+                            <Text className="text-zinc-950 dark:text-zinc-100">
+                                {
+                                    new Date(props.value).toLocaleDateString() +
+                                    (props.mode === "datetime" || props.mode === "time" ?
+                                        ' ' + new Date(props.value).toLocaleTimeString() :
+                                        ''
+                                    )
+                                }
+                            </Text> :
+                            <Text style={{ color: "grey" }}>
+                                {props.placeholder}
+                            </Text>
+                    }
+                </TouchableOpacity>
+            </InputGroup>
+            {
+                show && (
+                    <DateTimePicker
+                        id={props.id}
+                        value={new Date(props.value || Date.now())}
+                        onChange={onChangeDate}
+                        mode={props.mode}
+                    />
+                )
+            }
+        </>
+    );
+}
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,6 +2,7 @@ import Label from "./Label";
 import InputGroup from "./InputGroup";
 import InputText from "./InputText";
 import InputPassword from "./InputPassword";
+import InputDate from "./InputDate";
 import Button from "./Button";
 
-export { Label, InputGroup, InputText, InputPassword, Button };
+export { Label, InputGroup, InputText, InputPassword, InputDate, Button };
