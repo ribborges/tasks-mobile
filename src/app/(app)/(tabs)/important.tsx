6a7e69c20b714c83a3ffdc41778b05d8b557afdc
Richard Borges <ribborges@outlook.com>
2025-02-08 14:47:38 -0300

    feat: implement Important page with ScrollView and task filtering

--- src/app/(app)/(tabs)/important.tsx
+++ src/app/(app)/(tabs)/important.tsx
@@ -1,14 +1,29 @@
-import { View } from 'react-native';
+import { ScrollView, Text } from 'react-native';
 
-export default function C() {
+import Collapse from '@/components/Collapse';
+import { Blanckspace } from '@/components/Separator';
+import { TaskList } from '@/components/Task';
+import { useTaskStore } from '@/lib/store';
+
+export default function Important() {
+    const { tasks } = useTaskStore();
+
     return (
-        <View className="
+        <ScrollView className="
             flex-1
-            p-4
-            bg-zinc-200 dark:bg-zinc-900
+            bg-zinc-100 dark:bg-zinc-900
             border border-solid rounded-t-3xl
-            border-zinc-300 dark:border-zinc-800
-        ">
-        </View>
+            border-zinc-200 dark:border-zinc-800
+        " contentContainerClassName="gap-2 p-4">
+            <TaskList tasks={tasks.filter(task => task.status !== "completed" && task.isImportant)} />
+            <Collapse className="gap-2" titleClassName="text-indigo-500 font-bold text-xl" title="Completed">
+                {
+                    tasks.filter(task => task.status === "completed" && task.isImportant).length > 0 ?
+                        <TaskList tasks={tasks.filter(task => task.status === "completed" && task.isImportant)} /> :
+                        <Text className="text-lg text-gray-500">No completed tasks</Text>
+                }
+            </Collapse>
+            <Blanckspace space={80} />
+        </ScrollView>
     );
 }
